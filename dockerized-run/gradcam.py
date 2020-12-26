d1f7776e778a14768146ec9c391d634e412616c5
Ujjwal <ujjwalupadhyay8@gmail.com>
2020-12-26 17:42:57 +0530

    docker volumes

---
+++ dockerized-run/gradcam.py
@@ -0,0 +1,174 @@
+import time, os, sys
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+from keras.preprocessing.image import ImageDataGenerator
+from keras.applications.densenet import DenseNet121
+from keras.layers import Dense, GlobalAveragePooling2D
+from keras.models import Model, load_model
+from keras import backend as K
+
+from keras.models import load_model
+
+from flask import Flask, flash, request, redirect, url_for, send_file
+from werkzeug.utils import secure_filename
+
+import random
+import util
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+from keras import backend as K
+from keras.preprocessing import image
+from sklearn.metrics import roc_auc_score, roc_curve
+from tensorflow.compat.v1.logging import INFO, set_verbosity
+
+random.seed(a=None, version=2)
+
+set_verbosity(INFO)
+
+
+labels = ['Cardiomegaly',
+ 'Emphysema',
+ 'Effusion',
+ 'Hernia',
+ 'Infiltration',
+ 'Mass',
+ 'Nodule',
+ 'Atelectasis',
+ 'Pneumothorax',
+ 'Pleural_Thickening',
+ 'Pneumonia',
+ 'Fibrosis',
+ 'Edema',
+ 'Consolidation']
+
+# UNQ_C3 (UNIQUE CELL IDENTIFIER, DO NOT EDIT)
+def get_weighted_loss(pos_weights, neg_weights, epsilon=1e-7):
+    """
+    Return weighted loss function given negative weights and positive weights.
+
+    Args:
+      pos_weights (np.array): array of positive weights for each class, size (num_classes)
+      neg_weights (np.array): array of negative weights for each class, size (num_classes)
+    
+    Returns:
+      weighted_loss (function): weighted loss function
+    """
+    def weighted_loss(y_true, y_pred):
+        """
+        Return weighted loss value. 
+
+        Args:
+            y_true (Tensor): Tensor of true labels, size is (num_examples, num_classes)
+            y_pred (Tensor): Tensor of predicted labels, size is (num_examples, num_classes)
+        Returns:
+            loss (Float): overall scalar loss summed across all classes
+        """
+        # initialize loss to zero
+        loss = 0.0
+        
+        ### START CODE HERE (REPLACE INSTANCES OF 'None' with your code) ###
+
+        for i in range(len(pos_weights)):
+            # for each class, add average weighted loss for that class 
+            loss += -(K.mean((pos_weights[i] * y_true[:,i] * K.log(y_pred[:,i] + epsilon)) + (neg_weights[i] * (1-y_true[:,i]) * K.log(1-y_pred[:,i] + epsilon)),axis = 0)) #complete this line
+        return loss
+    
+        ### END CODE HERE ###
+    return weighted_loss
+
+
+neg_weights = np.array([0.02 , 0.013, 0.128, 0.002, 0.175, 0.045, 0.054, 0.106, 0.038, 0.021, 0.01 , 0.014, 0.016, 0.033])
+
+pos_weights = np.array([0.98 , 0.987, 0.872, 0.998, 0.825, 0.955, 0.946, 0.894, 0.962,0.979, 0.99 , 0.986, 0.984, 0.967])
+       
+df = pd.read_csv("train-small.csv")
+
+
+def get_mean_std_per_batch(image_path, df, H=320, W=320):
+    sample_data = []
+    for idx, img in enumerate(df.sample(100)["Image"].values):
+        # path = image_dir + img
+        sample_data.append(
+            np.array(image.load_img(image_path, target_size=(H, W))))
+
+    mean = np.mean(sample_data[0])
+    std = np.std(sample_data[0])
+    return mean, std
+
+
+def load_image(img, image_dir, df, preprocess=True, H=320, W=320):
+    """Load and preprocess image."""
+    img_path = image_dir + img
+    mean, std = get_mean_std_per_batch(img_path, df, H=H, W=W)
+    x = image.load_img(img_path, target_size=(H, W))
+    if preprocess:
+        x -= mean
+        x /= std
+        x = np.expand_dims(x, axis=0)
+    return x
+
+
+def grad_cam(input_model, image, cls, layer_name, H=320, W=320):
+    """GradCAM method for visualizing input saliency."""
+    y_c = input_model.output[0, cls]
+    conv_output = input_model.get_layer(layer_name).output
+    grads = K.gradients(y_c, conv_output)[0]
+
+    gradient_function = K.function([input_model.input], [conv_output, grads])
+
+    output, grads_val = gradient_function([image])
+    output, grads_val = output[0, :], grads_val[0, :, :, :]
+
+    weights = np.mean(grads_val, axis=(0, 1))
+    cam = np.dot(output, weights)
+
+    # Process CAM
+    cam = cv2.resize(cam, (W, H), cv2.INTER_LINEAR)
+    cam = np.maximum(cam, 0)
+    cam = cam / cam.max()
+    return cam
+
+
+def compute_gradcam(model, img, image_dir, df, labels, selected_labels,
+                    layer_name='bn'):
+    preprocessed_input = load_image(img, image_dir, df)
+    print(preprocessed_input.shape)
+    predictions = model.predict(preprocessed_input)
+
+    print("Loading original image")
+    #plt.figure(figsize=(15, 10))
+    #plt.subplot(151)
+    #plt.title("Original")
+    #plt.axis('off')
+    #plt.imshow(load_image(img, image_dir, df, preprocess=False), cmap='gray')
+
+    j = 1
+    for i in range(len(labels)):
+        if labels[i] in selected_labels:
+            print(f"Generating gradcam for class {labels[i]}")
+            gradcam = grad_cam(model, preprocessed_input, i, layer_name)
+            print(gradcam)
+            # plt.subplot(151 + j)
+            plt.figure(figsize=(15, 10))
+#            plt.title(f"{labels[i]}: p={predictions[0][i]:.3f}")
+            plt.axis('off')
+            plt.imshow(load_image(img, image_dir, df, preprocess=False),
+                       cmap='gray')
+            plt.imshow(gradcam, cmap='jet', alpha=min(0.5, predictions[0][i]))
+            j += 1
+            plt.savefig('images/out.png', bbox_inches='tight')
+#            plt.show()
+
+
+if __name__ == '__main__':
+    K.clear_session()
+    labels_to_show = np.array(['Cardiomegaly'], dtype='<U18')
+    model = load_model('test-model.h5', compile=False)
+    model.compile(optimizer='adam', loss=get_weighted_loss(pos_weights, neg_weights))
+
+    compute_gradcam(model, sys.argv[1], sys.argv[2], df, labels, labels_to_show)
+
