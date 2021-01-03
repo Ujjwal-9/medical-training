517a1d104c4ffbca66e45f8544c0cc64fd4bf556
Ujjwal <ujjwalupadhyay8@gmail.com>
2021-01-03 17:43:00 +0530

    black fornatter

--- dicom-demo/get_datasets.py
+++ dicom-demo/get_datasets.py
@@ -1,6 +1,6 @@
 #!/bin/env python
 
-'''
+"""
 Runtime executable, example of how we might use our little API to get images
 
 Copyright (c) 2017 Vanessa Sochat, Stanford University
@@ -23,7 +23,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 
-'''
+"""
 
 
 import requests
@@ -35,35 +35,43 @@ import sys
 import os
 
 
-
 def get_parser():
 
     parser = argparse.ArgumentParser(
-    description="download and query data from your json dataset API.")
+        description="download and query data from your json dataset API."
+    )
 
-    parser.add_argument("--output", 
-                        dest='output', 
-                        help="a folder to save output files to.", 
-                        type=str, 
-                        default=None)
+    parser.add_argument(
+        "--output",
+        dest="output",
+        help="a folder to save output files to.",
+        type=str,
+        default=None,
+    )
 
-    parser.add_argument('--images', 
-                        dest='images', 
-                        help="download images", 
-                        default=False, 
-                        action='store_true')
+    parser.add_argument(
+        "--images",
+        dest="images",
+        help="download images",
+        default=False,
+        action="store_true",
+    )
 
-    parser.add_argument('--show', 
-                        dest='show', 
-                        help="print the json data structure of the endpoint to the screen", 
-                        default=False, 
-                        action='store_true')
+    parser.add_argument(
+        "--show",
+        dest="show",
+        help="print the json data structure of the endpoint to the screen",
+        default=False,
+        action="store_true",
+    )
 
-    parser.add_argument("--uri", 
-                        dest='uri', 
-                        help="the github repo username/reponame", 
-                        type=str, 
-                        default="pydicom/dicom-cookies")
+    parser.add_argument(
+        "--uri",
+        dest="uri",
+        help="the github repo username/reponame",
+        type=str,
+        default="pydicom/dicom-cookies",
+    )
 
     return parser
 
@@ -80,29 +88,31 @@ def main():
     if args.output is None and args.show is False:
         parser.print_help()
 
-        print("\nSpecify to download data with --output or just print to screen with --show")
+        print(
+            "\nSpecify to download data with --output or just print to screen with --show"
+        )
         sys.exit(0)
 
-    username,reponame=parse_uri(args.uri)
+    username, reponame = parse_uri(args.uri)
 
-    base = "https://%s.github.io/%s" %(username,reponame)    
-    images_base = "%s/images" %(base)    
-    datasets_base = "%s/datasets" %(base)    
+    base = "https://%s.github.io/%s" % (username, reponame)
+    images_base = "%s/images" % (base)
+    datasets_base = "%s/datasets" % (base)
 
     # Does the user want images or datasets?
     url = datasets_base
-    images_only=False
+    images_only = False
     if args.images is True:
         url = images_base
-        images_only=True
+        images_only = True
 
     # Retrieve the data structure
     if args.show is False:
-        print("Retrieving %s" %(url))
+        print("Retrieving %s" % (url))
     result = requests.get(url).json()
 
     if args.show is False:
-        print("Found %s results" %(len(result['data'])))
+        print("Found %s results" % (len(result["data"])))
 
     output = None
     if args.output is not None:
@@ -114,45 +124,49 @@ def main():
             print_pretty(result)
     else:
         print("Saving datasets to output folder.")
-        for entity in result['data']:
-            save_dataset(base=output,
-                         entity=entity,
-                         images_only=images_only)
+        for entity in result["data"]:
+            save_dataset(base=output, entity=entity, images_only=images_only)
+
 
 # Helper Functions
 
+
 def parse_uri(uri):
-    return uri.split('/',1)
+    return uri.split("/", 1)
+
 
 def print_pretty(content):
-    print(json.dumps(content, indent=4, separators=(',', ': ')))
+    print(json.dumps(content, indent=4, separators=(",", ": ")))
 
-def download_file(from_url,to_file):
+
+def download_file(from_url, to_file):
     urllib.request.urlretrieve(from_url, to_file)
+
 
 def mkdir(path):
     if not os.path.exists(path):
         os.mkdir(path)
 
-def save_dataset(base,entity,images_only=False):
-    entity_base = "%s/%s" %(base,entity['id'])    
-    mkdir(entity_base) 
-    if images_only:    
-        gets = ['images']
-    else:
-        gets = ['images','texts']
-    for key,url in entity['links'].items():
-        if key in gets:
-            entity_content = "%s/%s" %(entity_base,key)
-            mkdir(entity_content) 
-            content = requests.get(url).json()
-            for image in content['data']:
-                if key in image:
-                    for k,v in image[key].items():
-                        print("Downloading %s for %s" %(k,entity['id']))
-                        output_file = "%s/%s" %(entity_content,k)
-                        download_file(v,output_file)
-          
 
-if __name__ == '__main__':
+def save_dataset(base, entity, images_only=False):
+    entity_base = "%s/%s" % (base, entity["id"])
+    mkdir(entity_base)
+    if images_only:
+        gets = ["images"]
+    else:
+        gets = ["images", "texts"]
+    for key, url in entity["links"].items():
+        if key in gets:
+            entity_content = "%s/%s" % (entity_base, key)
+            mkdir(entity_content)
+            content = requests.get(url).json()
+            for image in content["data"]:
+                if key in image:
+                    for k, v in image[key].items():
+                        print("Downloading %s for %s" % (k, entity["id"]))
+                        output_file = "%s/%s" % (entity_content, k)
+                        download_file(v, output_file)
+
+
+if __name__ == "__main__":
     main()
--- dockerized-run/gradcam.py
+++ dockerized-run/gradcam.py
@@ -29,20 +29,22 @@ random.seed(a=None, version=2)
 set_verbosity(INFO)
 
 
-labels = ['Cardiomegaly',
- 'Emphysema',
- 'Effusion',
- 'Hernia',
- 'Infiltration',
- 'Mass',
- 'Nodule',
- 'Atelectasis',
- 'Pneumothorax',
- 'Pleural_Thickening',
- 'Pneumonia',
- 'Fibrosis',
- 'Edema',
- 'Consolidation']
+labels = [
+    "Cardiomegaly",
+    "Emphysema",
+    "Effusion",
+    "Hernia",
+    "Infiltration",
+    "Mass",
+    "Nodule",
+    "Atelectasis",
+    "Pneumothorax",
+    "Pleural_Thickening",
+    "Pneumonia",
+    "Fibrosis",
+    "Edema",
+    "Consolidation",
+]
 
 # UNQ_C3 (UNIQUE CELL IDENTIFIER, DO NOT EDIT)
 def get_weighted_loss(pos_weights, neg_weights, epsilon=1e-7):
@@ -52,13 +54,14 @@ def get_weighted_loss(pos_weights, neg_weights, epsilon=1e-7):
     Args:
       pos_weights (np.array): array of positive weights for each class, size (num_classes)
       neg_weights (np.array): array of negative weights for each class, size (num_classes)
-    
+
     Returns:
       weighted_loss (function): weighted loss function
     """
+
     def weighted_loss(y_true, y_pred):
         """
-        Return weighted loss value. 
+        Return weighted loss value.
 
         Args:
             y_true (Tensor): Tensor of true labels, size is (num_examples, num_classes)
@@ -68,22 +71,67 @@ def get_weighted_loss(pos_weights, neg_weights, epsilon=1e-7):
         """
         # initialize loss to zero
         loss = 0.0
-        
+
         ### START CODE HERE (REPLACE INSTANCES OF 'None' with your code) ###
 
         for i in range(len(pos_weights)):
-            # for each class, add average weighted loss for that class 
-            loss += -(K.mean((pos_weights[i] * y_true[:,i] * K.log(y_pred[:,i] + epsilon)) + (neg_weights[i] * (1-y_true[:,i]) * K.log(1-y_pred[:,i] + epsilon)),axis = 0)) #complete this line
+            # for each class, add average weighted loss for that class
+            loss += -(
+                K.mean(
+                    (pos_weights[i] * y_true[:, i] * K.log(y_pred[:, i] + epsilon))
+                    + (
+                        neg_weights[i]
+                        * (1 - y_true[:, i])
+                        * K.log(1 - y_pred[:, i] + epsilon)
+                    ),
+                    axis=0,
+                )
+            )  # complete this line
         return loss
-    
+
         ### END CODE HERE ###
+
     return weighted_loss
 
 
-neg_weights = np.array([0.02 , 0.013, 0.128, 0.002, 0.175, 0.045, 0.054, 0.106, 0.038, 0.021, 0.01 , 0.014, 0.016, 0.033])
+neg_weights = np.array(
+    [
+        0.02,
+        0.013,
+        0.128,
+        0.002,
+        0.175,
+        0.045,
+        0.054,
+        0.106,
+        0.038,
+        0.021,
+        0.01,
+        0.014,
+        0.016,
+        0.033,
+    ]
+)
 
-pos_weights = np.array([0.98 , 0.987, 0.872, 0.998, 0.825, 0.955, 0.946, 0.894, 0.962,0.979, 0.99 , 0.986, 0.984, 0.967])
-       
+pos_weights = np.array(
+    [
+        0.98,
+        0.987,
+        0.872,
+        0.998,
+        0.825,
+        0.955,
+        0.946,
+        0.894,
+        0.962,
+        0.979,
+        0.99,
+        0.986,
+        0.984,
+        0.967,
+    ]
+)
+
 df = pd.read_csv("train-small.csv")
 
 
@@ -91,8 +139,7 @@ def get_mean_std_per_batch(image_path, df, H=320, W=320):
     sample_data = []
     for idx, img in enumerate(df.sample(100)["Image"].values):
         # path = image_dir + img
-        sample_data.append(
-            np.array(image.load_img(image_path, target_size=(H, W))))
+        sample_data.append(np.array(image.load_img(image_path, target_size=(H, W))))
 
     mean = np.mean(sample_data[0])
     std = np.std(sample_data[0])
@@ -132,42 +179,31 @@ def grad_cam(input_model, image, cls, layer_name, H=320, W=320):
     return cam
 
 
-def compute_gradcam(model, img, image_dir, df, labels, selected_labels,
-                    layer_name='bn'):
+def compute_gradcam(
+    model, img, image_dir, df, labels, selected_labels, layer_name="bn"
+):
     preprocessed_input = load_image(img, image_dir, df)
     print(preprocessed_input.shape)
     predictions = model.predict(preprocessed_input)
 
     print("Loading original image")
-    #plt.figure(figsize=(15, 10))
-    #plt.subplot(151)
-    #plt.title("Original")
-    #plt.axis('off')
-    #plt.imshow(load_image(img, image_dir, df, preprocess=False), cmap='gray')
 
-    j = 1
     for i in range(len(labels)):
         if labels[i] in selected_labels:
             print(f"Generating gradcam for class {labels[i]}")
             gradcam = grad_cam(model, preprocessed_input, i, layer_name)
             print(gradcam)
-            # plt.subplot(151 + j)
             plt.figure(figsize=(15, 10))
-#            plt.title(f"{labels[i]}: p={predictions[0][i]:.3f}")
-            plt.axis('off')
-            plt.imshow(load_image(img, image_dir, df, preprocess=False),
-                       cmap='gray')
-            plt.imshow(gradcam, cmap='jet', alpha=min(0.5, predictions[0][i]))
-            j += 1
-            plt.savefig('images/out.png', bbox_inches='tight')
-#            plt.show()
+            plt.axis("off")
+            plt.imshow(load_image(img, image_dir, df, preprocess=False), cmap="gray")
+            plt.imshow(gradcam, cmap="jet", alpha=min(0.5, predictions[0][i]))
+            plt.savefig("images/out.png", bbox_inches="tight")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     K.clear_session()
-    labels_to_show = np.array(['Cardiomegaly'], dtype='<U18')
-    model = load_model('test-model.h5', compile=False)
-    model.compile(optimizer='adam', loss=get_weighted_loss(pos_weights, neg_weights))
+    labels_to_show = np.array(["Cardiomegaly"], dtype="<U18")
+    model = load_model("test-model.h5", compile=False)
+    model.compile(optimizer="adam", loss=get_weighted_loss(pos_weights, neg_weights))
 
     compute_gradcam(model, sys.argv[1], sys.argv[2], df, labels, labels_to_show)
-
